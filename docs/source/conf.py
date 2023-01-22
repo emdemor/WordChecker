c48c23410f4c84754a151c0b016dda3acfd70ab4
Eduardo Morais <emdemor415@gmail.com>
2023-01-22 01:09:05 -0300

    Bump version: 0.0.1.rc0 → 0.0.1.rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ project = "Word Checker"  # Change app_name
 copyright = "2023, Eduardo Morais"
 author = "Eduardo Morais"
 release = "2023"
-version = "0.0.1.rc0"
+version = "0.0.1.rc1"
 
 
 # -- General configuration ---------------------------------------------------
